7d2deb1e154e25b67e3726e1faad80f02cdb07b8
Your Name <your_email@example.com>
2026-07-30 15:33:52 +0530

    reduxtoolkit

---
+++ day19-react-reduxtoolkit/src/features.jsx/counterSlice.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createSlice } from '@reduxjs/toolkit'
+
+const counterSlice = createSlice ({
+    name: "count",
+    initialState: {
+    count: 0,
+  },
+  reducers : {
+    increment: (state, action) => {
+      console.log("this is action", action);
+      state.count++;
+    },
+    decrement: (state) => {
+      state.count--;
+  }}
+})
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
